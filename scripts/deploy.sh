e765fa8f466c6825b61a36f622e6348270530039
folabia1 <27552121+folabia1@users.noreply.github.com>
2024-03-17 22:28:53 +0000

    deploy script | add link to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,3 +5,5 @@ git merge main
 git push
 
 git checkout main
+
+echo "Deploying via Cloud Build: https://console.cloud.google.com/cloud-build/builds;region=europe-west1?project=ordinal-stone-417516"
