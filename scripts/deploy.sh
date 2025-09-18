ccdf7f782d3f4c597786acef7f63908f4de6be0c
folabia1 <27552121+folabia1@users.noreply.github.com>
2025-09-18 18:17:53 +0100

    remove docker, server, and scripts

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-git checkout production
-
-git merge main
-
-git push
-
-git checkout main
-
-echo "Deploying via Cloud Build: https://console.cloud.google.com/cloud-build/builds;region=europe-west1?project=ordinal-stone-417516"
--- server/app.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const app = express();
-const port = 8080;
-
-app.use(express.static("public"));
-
-app.listen(port);
