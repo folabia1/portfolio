8884346a07ca3ce266303f037639e04057ec09c6
folabia1 <27552121+folabia1@users.noreply.github.com>
2025-09-23 16:37:02 +0100

    setup eslint with typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,12 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react/jsx-runtime", "plugin:react-hooks/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:@typescript-eslint/recommended"],
   ignorePatterns: ["dist", "docs", ".eslintrc.cjs", "*.html"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "@typescript-eslint"],
   rules: {
     "react/jsx-no-target-blank": "off",
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
