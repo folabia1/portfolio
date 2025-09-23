b941703a52cec8e51434f245bbecaa6265aae2c0
folabia1 <27552121+folabia1@users.noreply.github.com>
2025-09-23 06:02:54 +0100

    move desktop and mobile into their own folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { DesktopApp } from "./DesktopApp.jsx";
-import { MobileApp } from "./MobileApp.jsx";
+import { DesktopApp } from "./desktop/app/DesktopApp.jsx";
+import { MobileApp } from "./mobile/app/MobileApp.jsx";
 
 function isMobileDevice() {
-  console.log("checking");
   return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent) || window.matchMedia("(pointer: coarse)").matches;
 }
 
